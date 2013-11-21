6f37eace0f36e9103ac0ce8b3d6a7ab16119a351
Ignacio Fuentes <ignaciofuentes@outlook.com>
2013-11-21 14:08:54 -0500

    change

--- MvcApplication2/MvcApplication2/Controllers/ValuesController.cs
+++ MvcApplication2/MvcApplication2/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace MvcApplication2.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2d" };
         }
 
         // GET api/values/5
