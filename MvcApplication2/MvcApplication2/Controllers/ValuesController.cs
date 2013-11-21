cea88f1824975053e305d0d15cff0fd4f4d32167
Ignacio Fuentes <ignaciofuentes@outlook.com>
2013-11-21 15:02:21 -0500

    final touch

--- MvcApplication2/MvcApplication2/Controllers/ValuesController.cs
+++ MvcApplication2/MvcApplication2/Controllers/ValuesController.cs
@@ -16,7 +16,7 @@ namespace MvcApplication2.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2d" };
+            return new string[] { "Ignacio", "Todd" };
         }
 
         // GET api/values/5
